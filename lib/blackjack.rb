50da53269304c85ea25e3019302123d903015b62
Ay Has <ahashim983@yahoo.com>
2019-03-31 10:34:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -60,18 +60,3 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner(initial_round)
-  welcome
-  card_total = initial_round.to_i
-  #display_card_total(card_total)
-  loop do
-    display_card_total(card_total)
-    if card_total != nil &&  card_total > 21
-      end_game(card_total)
-      break
-    else
-      hit?(card_total)
-    end
-  end
-  #end_game(card_total)
-end

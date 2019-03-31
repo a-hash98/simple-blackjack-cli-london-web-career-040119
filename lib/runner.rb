46ccaa1e9d92e1fd6a81451288a8242a638064b8
Ay Has <ahashim983@yahoo.com>
2019-03-31 10:35:42 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,17 +1,3 @@
 require_relative "blackjack.rb"
 
-def runner(initial_round)
-  welcome
-  card_total = initial_round.to_i
-  #display_card_total(card_total)
-  loop do
-    display_card_total(card_total)
-    if card_total != nil &&  card_total > 21
-      end_game(card_total)
-      break
-    else
-      hit?(card_total)
-    end
-  end
-  end_game(card_total)
-end
+runner
